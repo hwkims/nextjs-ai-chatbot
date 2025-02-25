02ab360a41b35a7e9a2c903bac49154b284d854f
Hyunwoo <hwkims@naver.com>
2025-02-25 12:16:29 +0900

    Create mistral-client.ts

---
+++ lib/mistral-client.ts
@@ -0,0 +1,11 @@
+import { Mistral } from '@mistralai/mistralai';
+
+const apiKey = process.env.MISTRAL_API_KEY;
+
+if (!apiKey) {
+  throw new Error("MISTRAL_API_KEY is not set");
+}
+
+const client = new Mistral({ apiKey });
+
+export default client;
